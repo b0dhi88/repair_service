5caa96809bb66eeecd74cc35754e982a6d9127ff
Daniyar.m <dan.abdikalikov@gmailcom>
2026-02-22 10:19:35 +0000

    add js libs and template/css stubs

--- repair_service/settings.py
+++ repair_service/settings.py
@@ -58,7 +58,7 @@ AUTH_USER_MODEL = 'requests_app.User'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
